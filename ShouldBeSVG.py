14103f20ce826905b27d5db8d36a3f3fbb4835c4
AntiCompositeNumber <anticompositenumber@gmail.com>
2019-08-06 22:50:56 -0400

    Move code to functions, define versioning

--- ShouldBeSVG.py
+++ ShouldBeSVG.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 # coding: utf-8
 # SPDX-License-Identifier: Apache-2.0
 
@@ -17,43 +17,59 @@
    # See the License for the specific language governing permissions and
    # limitations under the License.
 
+version = 'ShouldBeSVG 0.2.1'
 
 import pywikibot
 from pywikibot import pagegenerators
 
+def getUsage(cat):
+
+    gen = pagegenerators.CategorizedPageGenerator(cat,recurse=1,namespaces=6)
+
+
+    # Generate a dictionary with diagrams that should be SVG.
+    usageCounts = {}
+
+    for page in gen:
+        mimetype = pywikibot.FilePage(page).latest_file_info.mime
+        if mimetype != 'image/svg+xml':
+            try:
+                usage = pywikibot.FilePage.globalusage(page)
+                l = len(list(usage))
+                usageCounts[page] = l
+            except (pywikibot.NoUsername, pywikibot.PageRelatedError):
+                print('Skipping', page)
+
+
+    # Sort from greatest to least
+    usageCountsSorted = sorted(usageCounts, key=usageCounts.__getitem__, reverse=True)
+
+    # Count the global usage for the top 200 files
+    i = 0
+    j = 200
+    gallery = 'Total number of scanned files: {}\n'.format(len(list(usageCounts)))
+    gallery += '<gallery showfilename=yes >\n'
+    for page in usageCountsSorted:
+        if i < j:
+            i += 1
+            url = page.full_url()
+            title = page.title()
+            count = usageCounts[page]
+            gallery += '{title}|{i}. Used {count} times.\n'.format(title=title, i=i, count=count)
+    gallery += '</gallery>\n[[Category:Images that should use vector graphics]]\n[[Category:Diagram images that should use vector graphics]]'
+    return gallery
+
+def savePage(target, text):
+    site = pywikibot.Site('commons', 'commons')
+    target.text = text
+    target.save(summary='Updating gallery (Bot) ({version})'.format(version=version), botflag=False) 
+
 site = pywikibot.Site('commons', 'commons')
 cat = pywikibot.Category(site, 'Category:Diagram images that should use vector graphics')
-gen = pagegenerators.CategorizedPageGenerator(cat,recurse=1,namespaces=6)
+target = pywikibot.Page(site, 'Top 200 diagram images that should use vector graphics')
+
+galleryWikitext = getUsage(cat)
+#savePage(target, galleryWikitext)
+print(galleryWikitext)
 
 
-# Generate a dictionary with diagrams that should be SVG.
-usageCounts = {}
-
-for page in gen:
-    mimetype = pywikibot.FilePage(page).latest_file_info.mime
-    if mimetype != 'image/svg+xml':
-        try:
-            usage = pywikibot.FilePage.globalusage(page)
-            l = len(list(usage))
-            usageCounts[page] = l
-        except (pywikibot.NoUsername, pywikibot.PageRelatedError):
-            print('Skipping', page)
-
-
-# Sort from greatest to least
-usageCountsSorted = sorted(usageCounts, key=usageCounts.__getitem__, reverse=True)
-
-# Count the global usage for the top 200 files
-i = 0
-j = 200
-gallery = 'Total number of scanned files: {}\n'.format(len(list(usageCounts)))
-gallery += '<gallery showfilename=yes >\n'
-for page in usageCountsSorted:
-    if i < j:
-        i += 1
-        url = page.full_url()
-        title = page.title()
-        count = usageCounts[page]
-        gallery += f'{title}|{i}. Used {count} times.\n'
-gallery += '</gallery>\n[[Category:Images that should use vector graphics]]\n[[Category:Diagram images that should use vector graphics]]'
-print(gallery)
