fbbcc3506dbd8789adbaa93ce724fbd18d608840
AntiCompositeNumber <anticompositenumber@gmail.com>
2019-08-19 19:58:40 -0400

    Switch to subprocess and prevent unnecessary email
    After the first week, I don't need to get emails for every job as it
    starts and stops. Since cron emails the output of the program, I only
    want output when something goes wrong. This commit also turns off emails
    from the grid unless there's an error. Wrapping everything in a try:
    except: raise probably isn't the best way to do this, but I couldn't
    find any other.

--- ShouldBeSVG-helper.py
+++ ShouldBeSVG-helper.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 """Determine which ShouldBeSVG task should be run when called by cron"""
-import os
+import subprocess
 from datetime import datetime
 
-#Grab the current weekday and hour as numbers and concatenate them.
+# Grab the current weekday and hour as numbers and concatenate them.
 dt = datetime.utcnow()
 now = int(dt.strftime('%u%H'))
 
-#Crontab entry: 11 2,10,18 * * * jlocal /data/project/anticompositebot/AntiCompositeBot/ShouldBeSVG-helper.py
-#Each key corresponds to a now value when the cronjob could be running.
-#Each value corresponds to a key in the reports dict in ShouldBeSVG.py
+# Crontab entry: 11 2,10,18 * * * jlocal
+#   /data/project/anticompositebot/AntiCompositeBot/ShouldBeSVG-helper.py
+
+# Each key corresponds to a now value when the cronjob could be running.
+# Each value corresponds to a key in the reports dict in ShouldBeSVG.py
 reports = {102: 'diagram', 110: 'graph', 118: 'math',
            202: 'text', 210: 'sport', 218: 'military_insignia',
            302: 'biology', 310: 'ribbon', 318: 'technology',
@@ -18,8 +20,8 @@ reports = {102: 'diagram', 110: 'graph', 118: 'math',
            602: 'coat_of_arms', 610: 'locator_map', 618: 'logo',
            702: 'map', 710: 'flag', 718: 'symbol_of_municipalities_in_Japan'}
 
-#Find the report we should be running and send that to the grid.
-#If there's no report to run this hour, raise an error.
+# Find the report we should be running and send that to the grid.
+# If there's no report to run this hour, raise an error.
 toolpath = '/data/project/anticompositebot/AntiCompositeBot/'
 
 try:
@@ -28,7 +30,8 @@ except KeyError:
     print("Check your timing, there's no report to run this hour.")
     raise
 else:
-    command = ('jsub -m e -j y -o {toolpath}/logs '
-               '{toolpath}/ShouldBeSVG.py {report}').format(report=report,
-                                                            toolpath=toolpath)
-    os.system(command)
+    command = ['jsub', '-m', 'n', '-j', 'y', '-N',
+               'ShouldBeSVG-{report}'.format(report=report), '-q',
+               '{toolpath}/ShouldBeSVG.py'.format(toolpath=toolpath), report]
+    subprocess.run(command, check=True, stdout=subprocess.PIPE,
+                   universal_newlines=True)
