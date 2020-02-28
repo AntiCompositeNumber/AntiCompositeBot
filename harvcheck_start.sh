6952be6a7d43cf5cbf0998f65835b1902ba8d7c9
AntiCompositeNumber <anticompositenumber@gmail.com>
2020-02-28 12:44:52 -0500

    Non-restart operation

--- harvcheck_start.sh
+++ harvcheck_start.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-/data/project/anticompositebot/AntiCompositeBot/venv/bin/python3 /data/project/anticompositebot/AntiCompositeBot/src/harvcheck.py --auto random --limit 25 --run
+if [ $1 == "continuous" ]; then
+    /data/project/anticompositebot/AntiCompositeBot/venv/bin/python3 /data/project/anticompositebot/AntiCompositeBot/src/harvcheck.py --auto random --run
+elif [ $1 == "job" ]; then
+    /data/project/anticompositebot/AntiCompositeBot/venv/bin/python3 /data/project/anticompositebot/AntiCompositeBot/src/harvcheck.py --auto query --limit 9 --run
+fi
