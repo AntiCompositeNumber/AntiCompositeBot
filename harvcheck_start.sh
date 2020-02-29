c5c0be08cf867bb47c275bd9a8695b526bbaac05
AntiCompositeNumber <anticompositenumber@gmail.com>
2020-02-29 12:14:19 -0500

    update config

--- harvcheck_start.sh
+++ harvcheck_start.sh
@@ -2,5 +2,5 @@
 if [ "$1" == "continuous" ]; then
     /data/project/anticompositebot/AntiCompositeBot/venv/bin/python3 /data/project/anticompositebot/AntiCompositeBot/src/harvcheck.py --auto random --run
 elif [ "$1" == "job" ]; then
-    /data/project/anticompositebot/AntiCompositeBot/venv/bin/python3 /data/project/anticompositebot/AntiCompositeBot/src/harvcheck.py --auto query --limit 9 --run
+    /data/project/anticompositebot/AntiCompositeBot/venv/bin/python3 /data/project/anticompositebot/AntiCompositeBot/src/harvcheck.py --auto quarry --limit 9 --run
 fi
