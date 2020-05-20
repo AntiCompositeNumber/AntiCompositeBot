16fc5fc1528ca576807696058d0173b66d43f5cf
AntiCompositeNumber <anticompositenumber@gmail.com>
2020-05-19 20:54:54 -0400

    Try with a subprocess instead

--- src/blcheck.py
+++ src/blcheck.py
@@ -20,6 +20,8 @@
 import pywikibot
 import re
 import itertools
+import subprocess
+import tempfile
 
 enwiki = pywikibot.Site("en", "wikipedia")
 metawiki = pywikibot.Site("meta", "meta")
@@ -30,32 +32,52 @@ def get_blacklist_regex():
     meta = pywikibot.Page(metawiki, "Spam blacklist")
 
     blacklist = [
-        line.partition("#")[0].replace(r"\x", r"\\x")
+        line.partition("#")[0].replace("/", r"\/")
         for line in itertools.chain(local.text.split("\n"), meta.text.split("\n"))
         if line.partition("#")[0].strip()
     ]
-    regex = r"".join([r"https?:\/\/[a-z0-9\-.]*(", r"|".join(blacklist), r")"])
-    return re.compile(regex, flags=re.S | re.I)
+    regex = r"".join([r"/https?:\/\/[a-z0-9\-.]*(", r"|".join(blacklist), r")/Si"])
+    return regex
 
 
-def get_site_list():
-    page = pywikibot.Page(enwiki, "User:Praxidicae/fntest")
+def get_site_list(page):
     regex = re.compile(r"\*\{\{LinkSummary\|(.*)\}\}")
     for line in page.text:
-        yield line, re.match(regex, line).group(1)
+        match = re.match(regex, line)
+        if match:
+            yield line, match.group(1)
 
 
-def check_site(regex, site):
-    """Returns true if site is blacklisted"""
-    return bool(re.search(regex, site)) if site else False
+def run_grep(regex, sites):
+    site_text = "\n".join(site[1] for site in sites)
+    with tempfile.NamedTemporaryFile(delete=False, mode="w") as f:
+        f.write(site_text)
+        temp = f.name
+    res = subprocess.run(
+        ["grep", "-Po", "-ef", "-", "-f", temp],
+        input="regex",
+        capture_output=True,
+        text=True,
+        check=True,
+    )
+    return set(res.stdout.split("\n"))
 
 
 def struck(line):
     return f"*<s>{line[1:]}</s>"
 
 
+def update_text(page, new_lines):
+    text = page.text
+    for line, new_line in new_lines:
+        text.replace(line, new_line)
+    return text
+
+
 def main():
     regex = get_blacklist_regex()
-    sites = get_site_list()
-    bl_sites = [(struck(line), site) for line, site in sites if check_site(regex, site)]
-    bl_sites
+    page = pywikibot.Page(enwiki, "User:Praxidicae/fntest")
+    sites = get_site_list(page)
+    bl_sites = run_grep(regex, sites)
+    new_lines = [(line, struck(line)) for line, site in sites if site in bl_sites]
+    print(new_lines)
