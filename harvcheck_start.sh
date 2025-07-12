b8f81f8ddeb5bbb11d527620aab0304cf0ace5d8
AntiCompositeNumber <git@anticomposite.net>
2025-07-12 18:18:32 -0400

    Remove harvcheck
    It's old, it doesn't work, and now the tests are failing.

--- harvcheck_start.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-if [ "$1" == "continuous" ]; then
-    /data/project/anticompositebot/AntiCompositeBot/venv/bin/python3 /data/project/anticompositebot/AntiCompositeBot/src/harvcheck.py --auto --run
-elif [ "$1" == "job" ]; then
-    /data/project/anticompositebot/AntiCompositeBot/venv/bin/python3 /data/project/anticompositebot/AntiCompositeBot/src/harvcheck.py --auto --limit --run
-fi
